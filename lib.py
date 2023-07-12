3bd47261c453e5d19804b2a3efd9ba855eeb7a41
beqachx <129542763+beqachx@users.noreply.github.com>
2023-07-12 23:42:14 +0400

    Update lib.py

--- lib.py
+++ lib.py
@@ -1,4 +1,4 @@
-# 751916880812044179
+# 3202168832377317591
 class QuadraticEquationSolver:
     def __init__(self, a, b, c):
         self.a = a
@@ -8,7 +8,7 @@ class QuadraticEquationSolver:
     def solve(self):
         d = self.b**2 - 4 * self.a * self.c
 
-        if d < 0:
+        if d <= 0:
             return []
 
         if d == 0:
